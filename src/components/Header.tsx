66628a0cca5c78febcd3ca66394221fd4cabf4c9
melion11 <gphooenix@gmail.com>
2023-04-28 00:01:59 +0300

    add components and css grid

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src={'https://w7.pngwing.com/pngs/733/575/png-transparent-logo-x5-retail-group-business-brand-business-leaf-text-retail.png'}
+                alt={'logo'}/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img
+                    src={'https://gamerwall.pro/uploads/posts/2022-08/1661206848_1-gamerwall-pro-p-peizazh-leto-oboi-1.jpg'}
+                    alt={'image sea'}/>
+            </div>
+
+            <div>
+                ava + desc
+            </div>
+
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
